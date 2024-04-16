5828a94f0b623339cea7abc6944b1b681003733b
Daniel <117715001+Danio4801@users.noreply.github.com>
2024-04-16 15:40:44 +0200

    Parking sensor with LEDs
    This project aims to simulate the behavior of parking sensors in cars. Instead of the standard approach where the closer an object is, the more LED lights illuminate, here we have the opposite relationship.
    
    When an object is close to the sensors, fewer LED lights light up on the display, signaling to the driver that they are approaching an obstacle. Conversely, as the object moves away from the sensors, more LED lights become visible, indicating a greater distance from the obstacle.
    In a future update of the code, I will reverse the logic of the lights and add logic responsible for generating sounds like in a real car, as well as mount a speaker on the board.
    
    Watch the lights in action here: https://youtu.be/LkFHq7YeRMg
    
    As a student encountering such a sensor for the first time and taking my first steps on the Arduino platform I utilized the knowledge and code snippets provided on the website: howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04, and I do not claim intellectual ownership of the code fragment responsible for calculating the distance.

---
+++ Lab 4 - parking sensor with LEDs/parking sensor with LEDs.ino
@@ -0,0 +1,47 @@
+const int trigPin = 2; // Def pinu do wysyłania sygnału ultradźwiękowego
+const int echoPin = 3; // Def pinu do odbierania sygnału ultradźwiękowego
+
+const int thresholdMultiplier = 4; //odleglosc - wartosc progrowa
+float duration, distance; // Deklaracja zmiennych do przechowywania czasu trwania impulsu oraz odległości
+
+void setup() 
+{
+    pinMode(trigPin, OUTPUT); // Ustawienie pinu trigPin jako wyjście
+    pinMode(echoPin, INPUT); // Ustawienie pinu echoPin jako wejście
+    Serial.begin(9600); // Inicjalizacja komunikacji szeregowej (do debugowania)
+
+    // Konfiguracja pinów LED
+    for (int i = 12; i > 4; i--) 
+    {
+        pinMode(i, OUTPUT); // Ustawienie pinów od 12 do 5 jako wyjścia (do sterowania diodami LED)
+    }
+}
+
+void loop() 
+{
+    digitalWrite(trigPin, LOW); // Wysłanie impulsu niskiego na pin trigPin
+    delayMicroseconds(2); // Poczekanie 2 mikrosekundy
+    digitalWrite(trigPin, HIGH); // Wysłanie impulsu wysokiego na pin trigPin
+    delayMicroseconds(10); // Poczekanie 10 mikrosekundy
+    digitalWrite(trigPin, LOW); // Wyłączenie impulsu na pinie trigPin
+
+    duration = pulseIn(echoPin, HIGH); // Pomiar czasu trwania impulsu na pinie echoPin
+    distance = (duration * 0.0340) / 2; // Obliczenie odległości na podstawie czasu trwania impulsu
+    Serial.print("Distance: "); // Wyświetlenie komunikatu "Distance: " w monitorze szeregowym
+    Serial.println(distance); // Wyświetlenie zmierzonej odległości w monitorze szeregowym
+
+    // Warunek dla mnożnika wartości progowej
+    for (int i = 1; i < 10; i++) 
+    {
+        if (distance > (thresholdMultiplier * i)) 
+        {
+            digitalWrite(4 + i, HIGH); // Włączenie diody LED, która odpowiada obecnemu wartości mnożnika
+        } 
+        else 
+        {
+            digitalWrite(4 + i, LOW); // Wyłączenie diody LED, która odpowiada obecnemu wartości mnożnika
+        }
+    }
+
+  delay(50); // Poczekanie 50 milisekund przed kolejną iteracją pętli loop()
+}
